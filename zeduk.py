c2ff341fb052a025e362593f8d5299a03e62b8e9
Zedikon <70371915+Zedikon@users.noreply.github.com>
2021-02-01 21:05:41 +0300

    Update zeduk.py

--- zeduk.py
+++ zeduk.py
@@ -5,11 +5,13 @@ import asyncio
 
 zeduk = discord.Client()
 
-# module
-time_string = time.strftime("Время: %H:%M:%S")
-citats = ['', '', '']
-mat = [""]
+# time
+delta = datetime.timedelta(hours=3, minutes=0)
+t = (datetime.datetime.now(datetime.timezone.utc) + delta)
+nowtime = t.strftime("[%d.%m.%Y %H:%M:%S]")
 
+# random
+randoms = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 @zeduk.event
 async def on_ready():
@@ -21,30 +23,30 @@ async def on_ready():
         if message.author == zeduk.user:
             return
 # ping
-        if message.content.startswith("z!пинг"):
-            await message.channel.send(f':alarm_clock:Пинг: {0}'.format(zeduk.latency),)
-        if message.content.startswith("z!Пинг"):
-            await message.channel.send(':alarm_clock:Пинг: {0}'.format(zeduk.latency))
+        if message.content.startswith("z!ping"):
+            await message.channel.send(f':alarm_clock:Ping: {0}'.format(zeduk.latency),)
+        if message.content.startswith("z!Ping"):
+            await message.channel.send(':alarm_clock:ping: {0}'.format(zeduk.latency))
 # avatar
-        if message.content.startswith("z!ава"):
-            await message.channel.send(f":camera_with_flash:Ваша аватарка:")
+        if message.content.startswith("z!ava"):
+            await message.channel.send(f":camera_with_flash:Your avatar:")
             await message.channel.send(message.author.avatar_url)
-        if message.content.startswith("z!Ава"):
-            await message.channel.send(f":camera_with_flash:Ваша аватарка:")
+        if message.content.startswith("z!Ava"):
+            await message.channel.send(f":camera_with_flash:Your avatar:")
             await message.channel.send(message.author.avatar_url)
 # time
-        if message.content.startswith("z!время"):
-            await message.channel.send(f"{time_string}")
-        if message.content.startswith("z!Время"):
-            await message.channel.send(f"{time_string} ")
+        if message.content.startswith("z!time"):
+            await message.channel.send(nowtime)
+        if message.content.startswith("z!Time"):
+            await message.channel.send(nowtime)
 # guild
-        if message.content.startswith("z!сервера"):
-            await message.channel.send(f"Количество серверов: {len(zeduk.guilds)}")
+        if message.content.startswith("z!server"):
+            await message.channel.send(f"Server: {len(zeduk.guilds)}")
 # random
-        if message.content.startswith("z!рандом"):
-            await message.channel.send(f":8ball:Вам выпало число:{random.choice(random4ik)}")
-        if message.content.startswith("z!Рандом"):
-            await message.channel.send(f":8ball:Вам выпало число:{random.choice(random4ik)}")
+        if message.content.startswith("z!Random"):
+            await message.channel.send(f":8ball:Random:{random.choice(randoms)}")
+        if message.content.startswith("z!random"):
+            await message.channel.send(f":8ball:Random:{random.choice(randoms)}")
 # delete message
         if message.content.startswith("z!delete"):
             await message.delete()
