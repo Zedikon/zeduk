85a92aca93578676ad10d823dc0a79144a485e39
Zedikon <70371915+Zedikon@users.noreply.github.com>
2021-03-21 16:18:55 +0300

    Update zeduk.py

--- zeduk.py
+++ zeduk.py
@@ -2,21 +2,28 @@ import discord
 import random
 import time
 import asyncio
+import datetime
 
 zeduk = discord.Client()
 
 # time
 delta = datetime.timedelta(hours=3, minutes=0)
 t = (datetime.datetime.now(datetime.timezone.utc) + delta)
-nowtime = t.strftime("[%d.%m.%Y %H:%M:%S]")
+nowtime = t.strftime("Дата: %d.%m.%Y Время: %H:%M:%S")
 
 # random
 randoms = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
+#embed
+embed = discord.Embed(
+    title='',
+    color=discord.Color.red())
+
 @zeduk.event
 async def on_ready():
     print('You login as: {0}!'.format(zeduk.user))
-    await zeduk.change_presence(activity=discord.Streaming(name="Префикс z!", url="your twitch ;)"))
+    while True:
+          await zeduk.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"за {len(zeduk.guilds)} серверами"))
 
     @zeduk.event
     async def on_message(message):
@@ -29,39 +36,42 @@ async def on_ready():
             await message.channel.send(':alarm_clock:ping: {0}'.format(zeduk.latency))
 # avatar
         if message.content.startswith("z!ava"):
-            await message.channel.send(f":camera_with_flash:Your avatar:")
-            await message.channel.send(message.author.avatar_url)
+            embed.add_field(name=f'Аватарка:', value=message.author.avatar_url)
+            await message.channel.send(embed=embed)
         if message.content.startswith("z!Ava"):
             await message.channel.send(f":camera_with_flash:Your avatar:")
             await message.channel.send(message.author.avatar_url)
 # time
         if message.content.startswith("z!time"):
-            await message.channel.send(nowtime)
+            embed.add_field(name=f'Время:', value=nowtime)
+            await message.channel.send(embed=embed)
         if message.content.startswith("z!Time"):
-            await message.channel.send(nowtime)
+            embed.add_field(name=f'Время:', value=nowtime)
+            await message.channel.send(embed=embed)
 # guild
         if message.content.startswith("z!server"):
             await message.channel.send(f"Server: {len(zeduk.guilds)}")
 # random
         if message.content.startswith("z!Random"):
-            await message.channel.send(f":8ball:Random:{random.choice(randoms)}")
+            embed.add_field(name=f':8ball:Рандом', value=random.choice(randoms))
+            await message.channel.send(embed=embed)
         if message.content.startswith("z!random"):
-            await message.channel.send(f":8ball:Random:{random.choice(randoms)}")
-# delete message
-        if message.content.startswith("z!delete"):
-            await message.delete()
-#hello
+            embed.add_field(name=f':8ball:Рандом', value=random.choice(randoms))
+            await message.channel.send(embed=embed)
+#newserver
 @zeduk.event
 async def on_guild_join(guild):
     category = guild.categories[0]
     channel = category.channels[0]
-    await channel.send("**привет я новый на этом сервере! мой префикс 'z!' что бы увидеть мой фунционал пропиши z!команды**")
+    embed.add_field(name=f'Новый сервер!', value="Привет! Спасибо что добавили меня на сервер! Пропишите z!help что бы посмотреть мой функционал!")
+    await newguild.send(embed=embed)
 
 # newmember
 @zeduk.event
 async def on_member_join(member):
-    privet = ["приветсвую тебя на нашем сервере!", "добро пожаловать на наш сервер!"]
-    await member.send(f'Привет {member}! **{random.choice(privet)}**')
+    hello = ["приветсвую тебя на нашем сервере!", "добро пожаловать на наш сервер!"]
+    embed.add_field(name=f'Привет!', value=random.choice(hello))
+    await member.send(embed=embed)
 
 
-zeduk.run('your token')
+zeduk.run('твой токен')
